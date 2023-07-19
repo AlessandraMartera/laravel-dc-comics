2827eb8e1013b3087d78808cd5f222b4ab45689b
alessandra martera <ale.martera@gmail.com>
2023-07-19 17:28:21 +0200

    upadate layout create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.layout-main')
 
 @section('content')
-    <form class="text-center" method="POST" action="{{ route('store') }}">
+    <form class="container" method="POST" action="{{ route('store') }}">
         @csrf
 
-        <div class="my-3">
-            <label for="title">Titolo</label>
-            <input type="text" name="title">
+        <div class="my-3 row">
+            <label class="col-5" for="title">Titolo</label>
+            <input class="col-3" type="text" name="title">
 
             @error('title')
                 <div class="alert alert-danger">{{ $message }}</div>
@@ -15,50 +15,50 @@
 
 
 
-        <div class="my-3">
-            <label for="description">Descrizione</label>
-            <input type="text" name="description">
+        <div class="my-3 row">
+            <label class="col-5" for="description">Descrizione</label>
+            <input class="col-3" type="text" name="description">
 
             @error('description')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="my-3">
-            <label for="thumb">Path immagine</label>
-            <input type="text" name="thumb">
+        <div class="my-3 row">
+            <label class="col-5" for="thumb">Path immagine</label>
+            <input class="col-3" type="text" name="thumb">
             @error('thumb')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="my-3">
-            <label for="price">Prezzo</label>
-            <input type="text" name="price">
+        <div class="my-3 row">
+            <label class="col-5" for="price">Prezzo</label>
+            <input class="col-3" type="text" name="price">
             @error('price')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="my-3">
-            <label for="series">Serie</label>
-            <input type="text" name="series">
+        <div class="my-3 row">
+            <label class="col-5" for="series">Serie</label>
+            <input class="col-3" type="text" name="series">
             @error('series')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="my-3">
-            <label for="sale_date">data di vedendità</label>
-            <input type="text" name="sale_date">
+        <div class="my-3 row">
+            <label class="col-5" for="sale_date">data di vedendità</label>
+            <input class="col-3" type="text" name="sale_date">
             @error('sale_date')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="my-3">
-            <label for="type">tipo</label>
-            <input type="text" name="type">
+        <div class="my-3 row">
+            <label class="col-5" for="type">tipo</label>
+            <input class="col-3" type="text" name="type">
             @error('type')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -8,37 +8,59 @@
         <div class="my-3 row">
             <label class="col-6" for="title">Titolo</label>
             <input class="col-4" type="text" name="title" id="title" value="{{ $comic->title }}">
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="my-3 row">
             <label class="col-6" for="description">Descrizione</label>
             <input class="col-4" type="text" name="description" id="description" value="{{ $comic->description }}">
+
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="my-3 row">
             <label class="col-6" for="thumb">Path immagine</label>
             <input class="col-4" type="text" name="thumb" id="thumb" value="{{ $comic->thumb }}">
+            @error('thumb')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
 
         <div class="my-3 row">
             <label class="col-6" for="price">Prezzo</label>
             <input class="col-4" type="text" name="price" id="price" value="{{ $comic->price }}">
+            @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="my-3 row">
             <label class="col-6" for="series">Serie</label>
             <input class="col-4" type="text" name="series" id="series" value="{{ $comic->series }}">
+            @error('series')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="my-3 row">
             <label class="col-6" for="sale_date">data di vedendità</label>
             <input class="col-4" type="text" name="sale_date" id="sale_date" value="{{ $comic->sale_date }}">
+            @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="my-3 row">
             <label class="col-6" for="type">tipo</label>
             <input class="col-4" type="text" name="type" id="type" value="{{ $comic->type }}">
+            @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
         <input class="my-5" type="submit" value="Aggiorna">
